225e04731506fb30238df459d151e2a5aa5058b9
scienceadda <85312366+scienceadda@users.noreply.github.com>
2021-08-13 23:28:36 +0600

    initial

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+// const ejs = require("ejs");
+const expressjslayouts = require("express-ejs-layouts");
+const path = require("path");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.use(expressjslayouts);
+app.set("views", path.join(__dirname, "/resources/view"));
+app.set("view engine", "ejs");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("listening on port http://localhost:" + PORT);
+});
