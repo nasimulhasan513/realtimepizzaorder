7ba7425bac390cca8d79d1482d1a7291cb6e31da
scienceadda <85312366+scienceadda@users.noreply.github.com>
2021-08-14 23:27:51 +0600

    add cart page and auth

--- server.js
+++ server.js
@@ -6,15 +6,22 @@ const path = require("path");
 const app = express();
 
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
 app.use(expressjslayouts);
 app.set("views", path.join(__dirname, "/resources/view"));
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/cart", (req, res) => {
+  res.render("customer/cart");
+});
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
