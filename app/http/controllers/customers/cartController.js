78265152ce3918a4fda673e346fba0345bf2f598
scienceadda <85312366+scienceadda@users.noreply.github.com>
2021-08-15 23:07:51 +0600

    add to cart

---
+++ app/http/controllers/customers/cartController.js
@@ -0,0 +1,26 @@
+const cartController = () => {
+  return {
+    index(req, res) {
+      res.render("customer/cart");
+    },
+    update(req, res) {
+      if (!req.session.cart) {
+        req.session.cart = { items: {}, totalQty: 0, totalPrice: 0 };
+      }
+
+      let cart = req.session.cart;
+
+      // check if exist
+      if (!cart.items[req.body._id]) {
+        cart.items[req.body._id] = { item: req.body, qty: 1 };
+      } else {
+        cart.items[req.body._id].qty += 1;
+      }
+      cart.totalQty += 1;
+      cart.totalPrice += req.body.price;
+      return res.json({ totalQty: cart.totalQty });
+    },
+  };
+};
+
+module.exports = cartController;
--- server.js
+++ server.js
@@ -1,28 +1,67 @@
+require("dotenv").config();
 const express = require("express");
-// const ejs = require("ejs");
 const expressjslayouts = require("express-ejs-layouts");
 const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes/web");
+const session = require("express-session");
+const flash = require("express-flash");
+const MongoDbStore = require("connect-mongo");
 
 const app = express();
 
+// db connection
+
+const url = "mongodb://localhost/pizza";
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+});
+
+const connection = mongoose.connection;
+
+connection
+  .once("open", () => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log("connection error");
+  });
+// session store
+let mongoStore = MongoDbStore.create({
+  mongoUrl: url,
+  collection: "sessions",
+});
+
+// session configuration
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: mongoStore,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+  })
+);
+
+// Global Middleware
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
+app.use(flash());
 app.use(express.static("public"));
+app.use(express.json());
 app.use(expressjslayouts);
 app.set("views", path.join(__dirname, "/resources/view"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/cart", (req, res) => {
-  res.render("customer/cart");
-});
-app.get("/login", (req, res) => {
-  res.render("auth/login");
-});
-app.get("/register", (req, res) => {
-  res.render("auth/register");
-});
-const PORT = process.env.PORT || 3000;
+routes(app);
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log("listening on port http://localhost:" + PORT);
